75fa0fb56fa6e1241e0fdf4cef0b961b01a2ffc6
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-13 17:57:51 -0300

    setting up axios

---
+++ src/components/DrinksList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'DrinksList',
+  components: {},
+  data: () => ({
+    drinks: [],
+
+  }),
+  methods:{
+    /*
+    gettingDrinks(){
+      try{
+        axios.get().then()
+      } catch () {
+
+      }
+    }
+  */
+  }
+
+}
+
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    <h1 class="text-blue-400">Hell World</h1>
+    <div class="flex flex-row justify-between items-center bg-black h-32">
+      <img class="h-12 w-22 ml-4" src="https://www.pngkey.com/png/full/429-4296716_drink-menu-derbys-bar-logo-white-drink-menu.png">
+      <div>
+        <ul class="flex flex-row justify-evenly w-96 text-white">
+          <li>Sobre</li>
+          <li>Bebidas</li>
+        </ul>
+      </div>
+    </div>
+    <div>
+
+    </div>
   </div>
 </template>
 
