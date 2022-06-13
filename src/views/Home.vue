bf84c3fe62417d67d34a148315e19cb4b980a9d2
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-13 16:15:35 -0300

    setting up tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <h1>Hell World</h1>
+    <h1 class="text-blue-400">Hell World</h1>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
 
 }
 </script>
