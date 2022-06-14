57376ae0f4ce01914a7d02780eb051b8baddab34
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-14 18:13:48 -0300

    starting sobre

--- src/components/Header/Navbar.vue
+++ src/components/Header/Navbar.vue
@@ -4,7 +4,9 @@
       <img class="h-12 w-22 ml-4" src="https://www.pngkey.com/png/full/429-4296716_drink-menu-derbys-bar-logo-white-drink-menu.png">
       <div>
         <ul class="flex flex-row justify-evenly w-96 text-white">
-          <li>Sobre</li>
+          <router-link to="/sobre">
+            <li>Sobre</li>
+          </router-link>
           <li>Bebidas</li>
         </ul>
       </div>
---
+++ src/views/Sobre.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'Sobre',
+    components: {},
+    data: () => ({
+
+    }),
+    methods: {
+
+    }
+  }
+</script>
