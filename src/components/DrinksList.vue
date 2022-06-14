8e939aca62dd4db1cb852642f8c589bd20c422be
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-14 16:08:18 -0300

    drinks layout

--- src/components/DrinksList.vue
+++ src/components/DrinksList.vue
@@ -1,63 +1,15 @@
 <template>
-  <div class="flex-col items-center justify-center mt-2">
-    <div class="flex flex-row items-center justify-evenly">
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+  <div>
+    <h1 class="mt-2 text-2xl">Lista de Bebidas</h1>
+    <div class="flex items-center justify-evenly mt-2">
+      <div class="flex flex-col items-center justify-center" v-for="drink in drinks" key="drink.id">
+        <div class="flex flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+          <div>
+            <img class="h-38 w-48" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+          </div>
+          <h1>{{ drink.name }}</h1>
+          <h2>{{ drink.description }}</h2>
         </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-    </div>
-    <div class="flex flex-row items-center justify-evenly mt-4">
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
-      </div>
-      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
-        <div>
-          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
-        </div>
-        <h1>Drink</h1>
-        <h2>R$</h2>
       </div>
     </div>
   </div>
