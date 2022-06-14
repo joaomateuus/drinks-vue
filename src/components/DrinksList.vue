b83ba272728603aecec527e7beb57a783afe8714
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-14 12:18:04 -0300

    starting drink list strucuture

--- src/components/DrinksList.vue
+++ src/components/DrinksList.vue
@@ -1,7 +1,64 @@
 <template>
-  <div>
-    <div>
-      <h1>Hello World</h1>
+  <div class="flex-col items-center justify-center mt-2">
+    <div class="flex flex-row items-center justify-evenly">
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+    </div>
+    <div class="flex flex-row items-center justify-evenly mt-4">
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
+      <div class="flex-col items-center justify-center h-fit w-72 border-2 border-black p-4">
+        <div>
+          <img class="h-38 w-48 ml-8" src="https://s2.glbimg.com/W2QRvvfeLt57STPhptJ7xdxO_yc=/smart/e.glbimg.com/og/ed/f/original/2020/12/21/receita-drinks-ceia-natal_10.jpg">
+        </div>
+        <h1>Drink</h1>
+        <h2>R$</h2>
+      </div>
     </div>
   </div>
 </template>
